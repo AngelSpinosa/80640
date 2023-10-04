67702b10e52aef7d70f446900b56ce54baee4225
AngelSpinosa <zs21016382@estudiantes.uv.mx>
2023-10-04 13:53:44 -0600

    Practica07

---
+++ Practica06.js
@@ -0,0 +1,7 @@
+const FORMULARIO = document.getElementById("button")
+    FORMULARIO.addEventListener("click", evento =>{
+        evento.preventDefault()
+    });
+    let u = document.getElementById("user")
+    let p = document.getElementById("password")
+    alert()
